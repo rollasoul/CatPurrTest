7e87fa1d629d53c6d97e767577850c44067dfdb3
rollasoul <roland@ouiouioui.space>
2020-07-19 21:01:41 +0200

    Initial Commit

---
+++ CatPurrTest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CatPurrTest
+//
+//  Created by Roland Arnoldt on 7/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
