367ae584293ed7da0216c727fefe9b4fe53d933c
rollasoul <roland@ouiouioui.space>
2020-07-19 21:46:48 +0200

    first commit

--- CatPurrTest/ContentView.swift
+++ CatPurrTest/ContentView.swift
@@ -6,10 +6,47 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
+    @State private var engine: CHHapticEngine?
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure the
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: 20)
+        events.append(event)
+        
+        // convert into pattern
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("didnt work: \(error.localizedDescription)")
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        Text("purr")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
     }
 }
 
